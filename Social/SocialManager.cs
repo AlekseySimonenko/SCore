f132c43aa5d2b232abefec0be8bfc007701e3030
Simonenko Alexey <alexey.simonenko@mail.ru>
2017-11-27 15:37:36 +0300

    Incapsulate ISocialPlatform

--- Social/SocialManager.cs
+++ Social/SocialManager.cs
@@ -15,7 +15,13 @@ namespace SCore
         #region Public var
         public ISocialPlatform platformAutoInit;
         public UnityEvent OnInitActions;
-        static public ISocialPlatform platform { get; private set; }
+        
+        static public event Callback.EventHandler InitCompletedEvent;
+        static public event Callback.EventHandler InitErrorEvent;
+        static public event Callback.EventHandler LoginEvent;
+        static public event Callback.EventHandler LogoutEvent;
+
+        static public bool loginCompleted { get { return platform.loginCompleted; } protected set { } }
         #endregion
 
         #region Public const
@@ -25,6 +31,7 @@ namespace SCore
         #endregion
 
         #region Private var
+        static private ISocialPlatform platform;
         #endregion
 
 
@@ -46,8 +53,14 @@ namespace SCore
             Debug.Log("SocialManager Init platform " + _platform.GetPlatformID());
             platform = _platform;
             platform.InitCompletedEvent += callbackCompleted;
-            platform.InitCompletedEvent += callbackError;
-            platform.Init();
+            platform.InitErrorEvent += callbackError;
+
+            platform.InitCompletedEvent += OnInitComleted;
+            platform.InitErrorEvent += OnInitErrorEvent;
+            platform.LoginEvent += OnLogin;
+            platform.LogoutEvent += OnLogout;
+
+        platform.Init();
         }
 
         void Update()
@@ -56,5 +69,69 @@ namespace SCore
         }
         #endregion
 
+
+        #region Static realisation of ISocialPlatform interface
+
+        static public void OnInitComleted()
+        {
+            if (InitCompletedEvent != null)
+                InitCompletedEvent();
+        }
+
+        static public void OnInitErrorEvent()
+        {
+            if (InitErrorEvent != null)
+                InitErrorEvent();
+        }
+
+        static public void OnLogin()
+        {
+            if (LoginEvent != null)
+                LoginEvent();
+        }
+
+        static public void OnLogout()
+        {
+            if (LogoutEvent != null)
+                LogoutEvent();
+        }
+
+
+        static public string GetPlatformID()
+        {
+            return platform.GetPlatformID();
+        }
+
+        static public void Login(Dictionary<string, object> parameters = null)
+        {
+            platform.Login(parameters);
+        }
+
+        static public void Logout()
+        {
+            platform.Logout();
+        }
+
+        static public string GetUserID()
+        {
+            return platform.GetUserID();
+        }
+
+        static public List<SocialUser> GetInAppFriends()
+        {
+            return platform.GetInAppFriends();
+        }
+
+        static public void InviteFriends(string inviteText = "")
+        {
+            platform.InviteFriends(inviteText);
+        }
+
+        static public void Share(string title, string message, string url, string imageUrl, Callback.EventHandler completedCallback, Callback.EventHandler errorCallback)
+        {
+            platform.Share(title, message, url, imageUrl, completedCallback, errorCallback);
+        }
+
+        #endregion
     }
 }
