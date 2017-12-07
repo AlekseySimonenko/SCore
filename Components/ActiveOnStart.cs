050ffc8944a4c257ad0ecc45050f6fb3d919f185
Simonenko Alexey <alexey.simonenko@mail.ru>
2017-12-07 14:20:47 +0300

    [add] Component ActiveOnStart - GameObject.SetActive()

---
+++ Components/ActiveOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveOnStart : MonoBehaviour {
+
+    public bool activeOnStart = true;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(activeOnStart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
