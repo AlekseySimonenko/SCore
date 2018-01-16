9ad9b82ecdc6dd4f0485306da778be87d2c630e8
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-01-16 17:50:55 +0300

    Change SetActivate component to Deactivator component conception

---
+++ Components/Deactivator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivator : MonoBehaviour
+{
+
+    public bool deactiveOnStart = false;
+    public bool deactiveOnUpdate = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.SetActive(!deactiveOnStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deactiveOnUpdate)
+            gameObject.SetActive(!deactiveOnUpdate);
+    }
+}
