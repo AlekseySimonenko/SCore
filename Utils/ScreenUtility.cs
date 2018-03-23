0ca697b160a33f766306c4a524e0c07662d04018
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-03-23 18:06:11 +0300

    Add screen Utility for tablet/phone detection

---
+++ Utils/ScreenUtility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace SCore
+{
+    public class ScreenUtility
+    {
+        public static float DeviceDiagonalSizeInInches()
+        {
+            float screenWidth = Screen.width / Screen.dpi;
+            float screenHeight = Screen.height / Screen.dpi;
+            float diagonalInches = Mathf.Sqrt(Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2));
+            return diagonalInches;
+        }
+
+        public static bool IsTablet()
+        {
+            return DeviceDiagonalSizeInInches() > 6.5F;
+        }
+    }
+}
