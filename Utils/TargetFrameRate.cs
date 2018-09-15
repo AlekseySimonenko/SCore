a3c41f6d3be9643e7851b17c0342ae9830da5648
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-09-15 15:49:50 +0300

    TargetFrameRate utils component

---
+++ Utils/TargetFrameRate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCore
+{
+    public class TargetFrameRate : MonoBehaviour
+    {
+        public int targetFrameRate = 60;
+        public bool inEditor = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (!Application.isEditor || inEditor)
+            {
+                // Limit the framerate to 60 to keep device from burning through cpu
+                Application.targetFrameRate = targetFrameRate;
+            }
+        }
+
+    }
+}
