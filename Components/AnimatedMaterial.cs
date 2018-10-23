1014ee38fa1695ffb50f627e2609d20acc1684f3
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-10-23 16:32:04 +0300

    Animated material component

---
+++ Components/AnimatedMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedMaterial : MonoBehaviour
+{
+
+    public string MaterialProperty = "";
+    public Material targetMaterial;
+    public float AnimationSpeed = 1f;
+    public bool UnscaledTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!string.IsNullOrEmpty(MaterialProperty) && targetMaterial != null)
+        {
+            targetMaterial.SetFloat(MaterialProperty, UnscaledTime ? Time.unscaledTime * AnimationSpeed : Time.time * AnimationSpeed);
+        }
+    }
+}
