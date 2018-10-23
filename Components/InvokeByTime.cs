29c1ee3ca6b7b872cc1b79e428704cb7a7c8ea2b
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-10-23 13:25:32 +0300

    InvokeByTime component added

---
+++ Components/InvokeByTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeByTime : MonoBehaviour
+{
+
+    public float TimeToInvoke;
+    public UnityEvent EventsToInvoke;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (TimeToInvoke == 0f)
+            InvokeEvents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeToInvoke > 0f)
+        {
+            TimeToInvoke -= Time.unscaledDeltaTime;
+            if (TimeToInvoke <= 0f)
+                InvokeEvents();
+        }
+    }
+
+    void InvokeEvents()
+    {
+        EventsToInvoke?.Invoke();
+    }
+}
