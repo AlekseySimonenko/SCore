6323c2409608dee59f60ed66815ec21a88d6ac01
Simonenko Alexey <alexey.simonenko@mail.ru>
2018-10-23 17:00:17 +0300

    Animated material for ui without shared material

--- Components/AnimatedMaterial.cs
+++ Components/AnimatedMaterial.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimatedMaterial : MonoBehaviour
 {
 
     public string MaterialProperty = "";
-    public Material targetMaterial;
+    public Image targetImage;
     public float AnimationSpeed = 1f;
     public bool UnscaledTime;
+
+    private Material targetMaterial;
 
     // Use this for initialization
     void Start()
     {
-
+        if (targetImage != null)
+        {
+            targetMaterial = new Material(targetImage.material);
+            targetImage.material = targetMaterial;
+        }
     }
 
     // Update is called once per frame
