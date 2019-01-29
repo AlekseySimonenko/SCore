0891bb244c8ac08bd723f793552378b559f45fcc
Simonenko Alexey <alexey.simonenko@mail.ru>
2019-01-29 16:03:58 +0300

    ActiveTargetByTime component added

---
+++ Components/ActiveTargetByTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace SCore
+{
+    /// <summary>
+    /// Activate or deactivate target (linked) gameobject by time
+    /// </summary>
+    public class ActiveTargetByTime : MonoBehaviour
+    {
+        public GameObject TargetObject;
+        public float activateOnTime = 0;
+        public bool deactivate = false;
+
+        void Start()
+        {
+            Invoke("ActivateObject", activateOnTime);
+        }
+
+        void Update()
+        {
+
+        }
+
+        void ActivateObject()
+        {
+            if (TargetObject != null)
+            {
+                TargetObject.SetActive(!deactivate);
+            }
+        }
+    }
+}
