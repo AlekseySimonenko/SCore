2cc9ffccec566698d0841ee94b65c892cc5e5a4e
Simonenko Alexey <alexey.simonenko@mail.ru>
2019-02-13 17:32:17 +0300

    Interface Single Installer added

---
+++ Framework/InterfaceSingleInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+public class InterfaceSingleInstaller : MonoBehaviour
+{
+    public Component BindedComponent;
+
+    private void Awake()
+    {
+        Type BindedInterface = BindedComponent.GetType().GetInterfaces()[0];
+        ProjectContext.Instance.Container.Bind(BindedInterface).FromInstance(BindedComponent).AsSingle();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
